1ff725eb7ec369303610594c5fe76d4baa97c9fb
vanhatai <vanhatai@gmail.com>
2021-10-13 10:04:47 +0200

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def game_core_v1(number:int=1) -> int:
+    count = 0
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
+            break # выход из цикла, если угадали
+    return(count)
+
+print(f'Количество попыток: {game_core_v1()}')
+
+def score_game(game_core_v1) -> int:
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+    Args:
+        random_predict ([type]): функция угадывания
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воcпроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+score_game(game_core_v1)
